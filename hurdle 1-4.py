a656422eb51c83502f26b620eba3d39e05169ac2
23091397 Rachman Tri M <149118391+RahmanTri@users.noreply.github.com>
2023-11-16 19:51:37 +0700

    Create hurdle 1-4.py

---
+++ hurdle 1-4.py
@@ -0,0 +1,38 @@
+# Hurdle 1
+# pembuatan beberapa fungsi
+# membuat fungsi belok kanan
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+# membuat fungsi loncat
+def loncat():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+# mulai eksekusi game
+move()
+loncat()
+move()
+loncat()
+move()
+loncat()
+move()
+loncat()
+move()
+loncat()
+move()
+loncat()
+
+# Hurdle 2
+# pembuatan beberapa fungsi
+# membuat fungsi belok kanan
+def turn_right():
+    turn_left()
+    turn_left()
