f79c4fda6d6307b82522b5c02daf1f7a1b68f8f2
RakaPhaedra <149665519+RakaPhaedra@users.noreply.github.com>
2023-11-16 20:33:46 +0700

    Update hurdle 1-4.py

--- hurdle 1-4.py
+++ hurdle 1-4.py
@@ -36,3 +36,40 @@ loncat()
 def turn_right():
     turn_left()
     turn_left()
+     turn_left()
+    
+# membuat fungsi loncat
+def loncat():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+# mulai eksekusi game
+# selama karter belum menyentuh bendera finish, maka program akan terus melakukan perulangan
+while at_goal()==False: 
+    move()
+    loncat()
+
+
+# Hurdle 3
+# pembuatan beberapa fungsi
+# membuat fungsi belok kanan
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+# membuat fungsi loncat
+def loncat():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
